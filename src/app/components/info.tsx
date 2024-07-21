278e0f77c1190098e79d48478b3a250c80003b03
aa6622 <aa6622@ic.ac.uk>
2024-07-21 04:48:32 +0100

    Fix: Changed value to volume

--- src/app/components/info.tsx
+++ src/app/components/info.tsx
@@ -16,9 +16,9 @@ export default function Info(props: InfoProps) {
   return (
     <div className="mt-5 overflow-auto" style={{ height: 275 }}>
       <p className="text-white">Wallet ID: {props.walletID}</p>
-      <p className="text-white">Value Inflow: {walletData.totalInflow}</p>
-      <p className="text-white">Value Outflow: {walletData.totalOutflow}</p>
-      <p className="text-white">Total Value: {walletData.totalInflow - walletData.totalOutflow}</p>
+      <p className="text-white">Volume Inflow: {walletData.totalInflow}</p>
+      <p className="text-white">Volume Outflow: {walletData.totalOutflow}</p>
+      <p className="text-white">Total Volume: {walletData.totalInflow - walletData.totalOutflow}</p>
       <p className="text-white">Transactions: {transactions.length} </p>
       <p className="text-white">Credibility Rating (Beta): TODO</p>
       <p className="text-white text-lg mt-2">Transactions</p>
