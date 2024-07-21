9a2a5448be8a9fad2a359c8137d99d1b1cd58b6c
Hardiv Harshakumar <hardivhk@gmail.com>
2024-07-21 02:32:16 +0100

    Update graph.tsx

--- src/app/components/graph.tsx
+++ src/app/components/graph.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { WalletData } from "@/backend/walletHistorySubscan";
 import { DataSet, Network } from "vis-network/standalone";
 
